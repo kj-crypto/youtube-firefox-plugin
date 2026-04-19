a190909853925c86034573c950122d42916fa20f
konrad <kjawulski@wp.pl>
2026-04-19 09:51:18 +0200

    WIP: plugin script

---
+++ plugin/copy_statics.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+EXCLUDE='.*\.\(ts\|js\|tsx\|jsx\)$'
+
+set_exclude() {
+    EXCLUDE=${1:-$EXCLUDE}
+}
+
+copy_statics() {
+    local source_dir=$1
+    local target_dir=$2
+    find "$source_dir" -type f ! -regex $EXCLUDE | while read -r file; do
+        dest="${file/#$source_dir/$target_dir}"
+        mkdir -p $(dirname "$dest")
+
+        cp "$file" "$dest"
+        echo "Updated $file"
+    done
+}
+
+watch_files() {
+    local source_dir=$1
+    local target_dir=$2
+    declare -A files_hash
+
+    while true; do
+        for file in $(find "$source_dir" -type f ! -regex $EXCLUDE); do
+            hash_=$(sha256sum "$file" | cut -d' ' -f1)
+            if [[ "${files_hash[$file]}" != "$hash_" ]]; then
+                files_hash[$file]=$hash_
+                dest="${file/#$source_dir/$target_dir}"
+                mkdir -p $(dirname "$dest")
+                cp "$file" "$dest"
+                echo "Updated $file"
+            fi
+        done
+    done
+}
+
+set_exclude
